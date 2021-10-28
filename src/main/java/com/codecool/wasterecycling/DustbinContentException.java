7e3f0d78377bc80cbe91ad5a0819ead9681f617d
LordBrutal <bogar.jozsef@gmail.com>
2021-10-28 13:24:32 +0200

    Add: method to DustbinContentException

--- src/main/java/com/codecool/wasterecycling/DustbinContentException.java
+++ src/main/java/com/codecool/wasterecycling/DustbinContentException.java
@@ -1,4 +1,8 @@
 package com.codecool.wasterecycling;
 
 public class DustbinContentException extends Exception {
+
+        public DustbinContentException() {
+                System.out.println("\nYou shouldn't throw this garbage into the dustbin!");
+        }
 }
