6b69dec27b89e23dd95ae683e0d6cc7f7e75b760
LordBrutal <bogar.jozsef@gmail.com>
2021-10-28 13:25:52 +0200

    Add: driver code

--- src/main/java/com/codecool/wasterecycling/Main.java
+++ src/main/java/com/codecool/wasterecycling/Main.java
@@ -3,5 +3,20 @@ package com.codecool.wasterecycling;
 public class Main {
 
     public static void main(String[] args) {
+
+            Dustbin dustbin = new Dustbin("Purple");
+            Garbage newGarbage = new Garbage("Half eaten sandwich");
+            dustbin.throwOutGarbage(newGarbage);
+            Garbage newGarbage2 = new Garbage("Rotten tomato");
+            dustbin.throwOutGarbage(newGarbage2);
+            PaperGarbage paperGarbage = new PaperGarbage("Failed exam", false);
+            dustbin.throwOutGarbage(paperGarbage);
+            PlasticGarbage plasticGarbage = new PlasticGarbage("Empty bottle of oil", false);
+            plasticGarbage.clean();
+            dustbin.throwOutGarbage(plasticGarbage);
+            dustbin.displayContents();
+            dustbin.emptyContents();
+            dustbin.displayContents();
+
     }
 }
