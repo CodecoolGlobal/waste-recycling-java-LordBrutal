78307c234bb2a926d4a8ce040ce97b4705b0a240
LordBrutal <bogar.jozsef@gmail.com>
2021-10-28 12:49:41 +0200

    Add: PaperGarbage specific field and methods

--- src/main/java/com/codecool/wasterecycling/PaperGarbage.java
+++ src/main/java/com/codecool/wasterecycling/PaperGarbage.java
@@ -1,4 +1,19 @@
 package com.codecool.wasterecycling;
 
 public class PaperGarbage extends Garbage {
+
+        private boolean squeezed;
+
+        public PaperGarbage (String name, Boolean squeezed) {
+                super(name);
+                this.squeezed = squeezed;
+        }
+
+        public void squeeze () {
+                squeezed  = true;
+        }
+
+        public boolean isSqueezed () {
+                return squeezed;
+        }
 }
