985c303c314020acdf9b6678d03b58480dbddf41
LordBrutal <bogar.jozsef@gmail.com>
2021-10-28 12:57:21 +0200

    Add: PlasticGarbage specific field and methods

--- src/main/java/com/codecool/wasterecycling/PlasticGarbage.java
+++ src/main/java/com/codecool/wasterecycling/PlasticGarbage.java
@@ -1,4 +1,19 @@
 package com.codecool.wasterecycling;
 
 public class PlasticGarbage extends Garbage {
+
+        private boolean clean;
+
+        public PlasticGarbage(String name, Boolean clean) {
+                super(name);
+                this.clean = clean;
+        }
+
+        public void clean () {
+                clean = true;
+        }
+
+        public boolean isClean () {
+                return clean;
+        }
 }
